2bd36462553927906035f405100ecb439319d735
velmanharefa <velmanh24@gmail.com>
2024-06-24 11:24:49 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/index', function () {
+    return view('index');
+});
+
+
+Route::get('/event', function () {
+    return view('event');
+});
+
 Route::get('/test', function () {
     return response()->json(['message' => 'Hello, world!']);
 });
